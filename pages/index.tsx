faf8f3aedd19ada70546a036d6d2f613b5bcbf77
ihossain-6 <ismumhossain@gmail.com>
2023-02-22 13:23:24 +0600

    Initial commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,55 +1,44 @@
-import { ConnectWallet } from "@thirdweb-dev/react";
+import { ConnectWallet, ThirdwebNftMedia, useAddress, useContract, useNFTs, Web3Button } from "@thirdweb-dev/react";
 import type { NextPage } from "next";
 import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
+  const {contract} = useContract (
+    "0x58cC14CEc98d6E81374410606138E60A6B875674"
+  )
+  const {data: nfts} = useNFTs(contract)
+
   return (
-    <div className={styles.container}>
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="http://thirdweb.com/">thirdweb</a>!
-        </h1>
-
-        <p className={styles.description}>
-          Get started by configuring your desired network in{" "}
-          <code className={styles.code}>pages/_app.tsx</code>, then modify the{" "}
-          <code className={styles.code}>pages/index.tsx</code> file!
-        </p>
-
-        <div className={styles.connect}>
-          <ConnectWallet />
+    <div>
+      <div className={styles.buttoncontainer}>
+      <ConnectWallet accentColor="purple" colorMode="light" className={styles.button}/>
+      </div>
+      
+      <hr />
+      <div className={styles.cardcontainer}>
+      {nfts?.map((nft) => (
+        <div className={styles.card} key={nft.metadata.id.toString()}>
+          <ThirdwebNftMedia metadata={nft.metadata} />
+          <h2>{nft.metadata.name}</h2>
         </div>
+      ))}
+      </div>
+      <hr />
+      <div className={styles.buttoncontainer}>
+      <Web3Button className={styles.button}
+      contractAddress="0x58cC14CEc98d6E81374410606138E60A6B875674"
+      action={(contract) => contract.erc1155.claim(0, 1)}
+      onSuccess={(result) => alert("Success!")}
+      >Claim a baby turtle</Web3Button>
 
-        <div className={styles.grid}>
-          <a href="https://portal.thirdweb.com/" className={styles.card}>
-            <h2>Portal &rarr;</h2>
-            <p>
-              Guides, references and resources that will help you build with
-              thirdweb.
-            </p>
-          </a>
-
-          <a href="https://thirdweb.com/dashboard" className={styles.card}>
-            <h2>Dashboard &rarr;</h2>
-            <p>
-              Deploy, configure and manage your smart contracts from the
-              dashboard.
-            </p>
-          </a>
-
-          <a
-            href="https://portal.thirdweb.com/templates"
-            className={styles.card}
-          >
-            <h2>Templates &rarr;</h2>
-            <p>
-              Discover and clone template projects showcasing thirdweb features.
-            </p>
-          </a>
-        </div>
-      </main>
+      <Web3Button className={styles.button}
+      contractAddress="0x58cC14CEc98d6E81374410606138E60A6B875674"
+      action={(contract) => contract.erc1155.claim(1, 1)}
+      onSuccess={(result) => alert("Success!")}
+      >Claim a tortoise</Web3Button>
+      </div>
     </div>
-  );
+  )
 };
 
 export default Home;

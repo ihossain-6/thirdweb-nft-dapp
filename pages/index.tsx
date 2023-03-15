0ba72a8c771f4285253ff587998c0e514b59db3d
ihossain-6 <ismumhossain@gmail.com>
2023-03-15 13:48:05 +0600

    Add responsiveness

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,44 +1,65 @@
 import { ConnectWallet, ThirdwebNftMedia, useAddress, useContract, useNFTs, Web3Button } from "@thirdweb-dev/react";
 import type { NextPage } from "next";
-import styles from "../styles/Home.module.css";
+import { Container, Row, Col, Card } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const Home: NextPage = () => {
-  const {contract} = useContract (
+  const { contract } = useContract(
     "0x58cC14CEc98d6E81374410606138E60A6B875674"
-  )
-  const {data: nfts} = useNFTs(contract)
+  );
+  const { data: nfts } = useNFTs(contract);
 
   return (
     <div>
-      <div className={styles.buttoncontainer}>
-      <ConnectWallet accentColor="purple" colorMode="light" className={styles.button}/>
+      <div className="d-flex justify-content-center my-3">
+        <ConnectWallet
+          accentColor="purple"
+          colorMode="light"
+        />
       </div>
-      
-      <hr />
-      <div className={styles.cardcontainer}>
-      {nfts?.map((nft) => (
-        <div className={styles.card} key={nft.metadata.id.toString()}>
-          <ThirdwebNftMedia metadata={nft.metadata} />
-          <h2>{nft.metadata.name}</h2>
-        </div>
-      ))}
-      </div>
-      <hr />
-      <div className={styles.buttoncontainer}>
-      <Web3Button className={styles.button}
-      contractAddress="0x58cC14CEc98d6E81374410606138E60A6B875674"
-      action={(contract) => contract.erc1155.claim(0, 1)}
-      onSuccess={(result) => alert("Success!")}
-      >Claim a baby turtle</Web3Button>
 
-      <Web3Button className={styles.button}
-      contractAddress="0x58cC14CEc98d6E81374410606138E60A6B875674"
-      action={(contract) => contract.erc1155.claim(1, 1)}
-      onSuccess={(result) => alert("Success!")}
-      >Claim a tortoise</Web3Button>
+      <hr />
+      <Container fluid>
+        <Row xs={1} sm={2} md={3} className="g-4">
+          {nfts?.map((nft) => (
+            <Col key={nft.metadata.id.toString()}>
+              <Card className="h-100">
+                <ThirdwebNftMedia metadata={nft.metadata} className="img-fluid" style={{ maxHeight: "12rem" }} />
+                <Card.Body>
+                  <Card.Title>{nft.metadata.name}</Card.Title>
+                </Card.Body>
+              </Card>
+            </Col>
+          ))}
+        </Row>
+      </Container>
+      <hr />
+      <div className="d-flex justify-content-center my-3">
+        <Container fluid>
+          <Row xs={1} sm={2} md={2} className="g-2 justify-content-md-center">
+            <Col>
+              <Web3Button
+                contractAddress="0x58cC14CEc98d6E81374410606138E60A6B875674"
+                action={(contract) => contract.erc1155.claim(0, 1)}
+                onSuccess={(result) => alert("Success!")}
+              >
+                Claim a baby turtle
+              </Web3Button>
+            </Col>
+            <Col>
+              <Web3Button
+                contractAddress="0x58cC14CEc98d6E81374410606138E60A6B875674"
+                action={(contract) => contract.erc1155.claim(1, 1)}
+                onSuccess={(result) => alert("Success!")}
+              >
+                Claim a tortoise
+              </Web3Button>
+            </Col>
+          </Row>
+        </Container>
       </div>
     </div>
-  )
+  );
 };
 
 export default Home;
